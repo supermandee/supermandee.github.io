2bc1cf85547cc8cfac7ab417aece85892ac65e98
supermandee <71103135+supermandee@users.noreply.github.com>
2025-01-21 23:22:19 -0600

    emaillist

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -26,6 +26,17 @@ export default function Footer() {
   return (
     <footer className="mb-16">
       <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
+      <li>
+          <a
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://docs.google.com/forms/d/e/1FAIpQLSdtlCVbqbMjcTMw23w5Sv5p44Csnz7w85mrAU3w_S_ZmdxUqg/viewform?usp=sharing"
+          >
+            <ArrowIcon />
+            <p className="ml-2 h-7">subscribe</p>
+          </a>
+        </li>
         <li>
           <button
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
