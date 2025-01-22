d5a4b1b6ff21e17ea5d58b121687c47f043bf52b
supermandee <71103135+supermandee@users.noreply.github.com>
2025-01-21 21:21:59 -0600

    ga4

---
+++ app/GoogleAnalytics.jsx
@@ -0,0 +1,24 @@
+// components/GoogleAnalytics.jsx
+'use client'
+
+import Script from 'next/script'
+
+export default function GoogleAnalytics() {
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-5LYV6F637Q"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-5LYV6F637Q');
+        `}
+      </Script>
+    </>
+  )
+}
