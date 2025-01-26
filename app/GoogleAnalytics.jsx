24fbf85aa25e2247d9abf62ed651d26055da6294
supermandee <71103135+supermandee@users.noreply.github.com>
2025-01-26 02:58:19 -0600

    gtm

--- app/GoogleAnalytics.jsx
+++ app/GoogleAnalytics.jsx
@@ -1,4 +1,3 @@
-// components/GoogleAnalytics.jsx
 'use client'
 
 import Script from 'next/script'
@@ -6,6 +5,7 @@ import Script from 'next/script'
 export default function GoogleAnalytics() {
   return (
     <>
+      {/* Google Analytics Script */}
       <Script
         src="https://www.googletagmanager.com/gtag/js?id=G-5LYV6F637Q"
         strategy="afterInteractive"
@@ -19,6 +19,27 @@ export default function GoogleAnalytics() {
           gtag('config', 'G-5LYV6F637Q');
         `}
       </Script>
+
+      {/* Google Tag Manager Script in <head> */}
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': 
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id=' + i + dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-K2TJKKM9');
+        `}
+      </Script>
+
+      {/* Google Tag Manager <noscript> */}
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-K2TJKKM9"
+          height="0"
+          width="0"
+          style={{ display: 'none', visibility: 'hidden' }}
+        />
+      </noscript>
     </>
   )
 }
